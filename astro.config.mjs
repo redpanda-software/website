3520e0d807016b2aa0777e9f211e754bf62175a6
Yannick <yannick@redpanda.ch>
2024-12-14 17:50:11 +0100

    configuring the basics for the blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://redpanda.ch',
 	integrations: [mdx(), sitemap()],
+  redirects: {
+    '/': '/blog'
+  }	
 });
